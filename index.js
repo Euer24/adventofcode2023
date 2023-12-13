290f00b3ae1bf3e315a9b57a86eb3d9e4d4c884b
Euer <euer2017@gmail.com>
2023-12-13 16:23:02 +0100

    dag 1 af

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
-
 function combineNumbers(number) {
-    // Verwijder alles wat geen nummer is in de tekst.
-    const numberString = number.replace(/[^0-9]/g, '');
-    // Controleer of er minimaal twee cijfers zijn
+   
+    if (number.length === 1) {
+        return parseInt(number + number, 10);
+    }
     
     // Pak de eerste en laatste cijfer.
-    const firstNumber = numberString[0];
-    const lastNumber = numberString[numberString.length - 1];
+    const firstNumber = number[0];
+    const lastNumber = number[number.length - 1];
     // Combineer de eerste en laatste nummer.
     const combined = parseInt(firstNumber + lastNumber, 10)
     // Geef de result terug
     return combined;
 }
-
+// Gooi hier je hier je input in.
 const text = [
-    "35557"
-   
+  "1",
+  
 ];
 
 let totalCombined = 0;
 
 text.forEach((text) => {
-    const answer = combineNumbers(text);
-    if (answer !== null) {
-        totalCombined += answer;
+    // Filter de tekst om alleen de cijfers te behouden
+    const numbersOnly = text.replace(/\D/g, '');
+    if (numbersOnly.length > 0) {
+        const answer = combineNumbers(numbersOnly);
+        if (answer !== null) {
+            totalCombined += answer;
+        }
     }
 });
 
 console.log("Totale combinatie: " + totalCombined);
+
+document.getElementById("ans").addEventListener("click", function() {
+    alert("Het antwoord is: " + totalCombined);
+});
+
